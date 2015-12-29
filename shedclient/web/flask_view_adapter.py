ed4cfb7cffb285b51584b02de58e18a237fc7f59
John Chilton <jmchilton@gmail.com>
2015-12-29 18:31:32 +0000

    Improvements to flask view adapter.

--- shedclient/web/flask_view_adapter.py
+++ shedclient/web/flask_view_adapter.py
@@ -1,11 +1,15 @@
 import json
 import os
 try:
-    from flask import Flask
-    from flask import send_from_directory
+    from flask import (
+        Flask,
+        send_from_directory,
+        request,
+    )
 except ImportError:
     Flask = None
     send_from_directory = None
+    request = None
 
 web_folder = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 packed_folder = os.path.join(web_folder, "packed")
@@ -29,9 +33,13 @@ def index():
     return send_from_directory(web_folder, 'index.html')
 
 
-@app.route('/shed_tool_conf', methods=['GET'])
-def get_shed_tool_conf():
-    return json.dumps(managed_tool_conf_view.get())
+@app.route('/shed_tool_conf', methods=['GET', 'PUT'])
+def shed_tool_conf():
+    if request.method == 'PUT':
+        return_json = managed_tool_conf_view.put(request.form_data)
+    else:
+        return_json = managed_tool_conf_view.get()
+    return json.dumps(return_json)
 
 
 if __name__ == '__main__':
